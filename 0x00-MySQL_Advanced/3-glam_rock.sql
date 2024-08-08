6832520611b23693cd35c536cef3eaab04d53d69
DENNYYESSAR <dennisokeri5@gmail.com>
2024-08-08 23:13:36 +0000

    Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,19 +1,6 @@
 -- Lists all bands with Glam rock as their main style, ranked by their longevity.
 -- SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
-CREATE VIEW country_fan_count AS
-SELECT 
-    origin, 
-    SUM(fans) AS nb_fans
-FROM 
-    bands
-GROUP BY 
-    origin
-ORDER BY 
-    nb_fans DESC;
-
--- Select the results from the view
-SELECT 
-    origin, 
-    nb_fans
-FROM 
-    country_fan_count;
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
